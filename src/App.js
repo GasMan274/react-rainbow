ee4a93838f1fa80c1f6d702f39acc93e541c4fd4
lawrencegraham <lgraham274@yahoo.com>
2022-10-06 21:46:02 -0400

    added rainbow route first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import ColorBlock from './colorblock';
+import ColorForm from './colorForm';
+
+function App() {
+
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
+  }
+
+  let [colors, setColors] = useState([
+    'violet', 'blue',
+    'lightblue', 'green',
+    'greenyellow', 'yellow',
+    'orange', 'red'
+  ])
+
+  let colorMap = colors.map((color, i) => {
+    return (
+      <ColorBlock color={color} key={i} />
+    )
+  })
+
+  return (
+    <div className="App">
+      {colorMap}
+      <ColorForm addColor={addColor} />
+    </div>
+  )
+
+}
+
+export default App;
